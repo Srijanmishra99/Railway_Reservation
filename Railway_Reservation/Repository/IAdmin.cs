bcce4bf9f17b9e96c6149b03187b0e750ceccd18
Srijan Mishra <Srijan Mishra@DESKTOP-2DS23JJ>
2023-10-05 23:04:17 +0530

    Add project files.

---
+++ Railway_Reservation/Repository/IAdmin.cs
@@ -0,0 +1,18 @@
+﻿using Railway_Reservation.Model;
+
+namespace Railway_Reservation.Repository
+
+    {
+        public class IAdmin : IAdminInterface
+        {
+            private List<Admin> _admins = new List<Admin>
+        {
+            new Admin { UserId = 1, PhoneNo = "8528041212", Password = "8528041212" }
+        };
+            public Admin Authenticate(string PhoneNo, string Pass)
+            {
+                return _admins.FirstOrDefault(u => u.PhoneNo.Equals(PhoneNo) && u.Password.Equals(Pass));
+            }
+        }
+   }
+
